f4074d84ac109b98973d1bf98d3f8aed791c742f
Timofey_V <tim_vi_ov@mail.ru>
2024-02-01 20:44:55 +0300

    add file my_command

---
+++ myproject/hw2/management/commands/my_command.py
@@ -0,0 +1,9 @@
+import django.core.management.base
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Print 'Hello world!' to output."
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write('Hello world!')

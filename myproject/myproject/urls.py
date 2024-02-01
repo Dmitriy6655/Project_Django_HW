14dafe69d7716442541cfedbaded17a46579e888
Timofey_V <tim_vi_ov@mail.ru>
2024-02-01 19:36:18 +0300

    add application hw1

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from hw1 import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index),
+    path('prefix/', include('hw1.urls')),
+    path('about/', views.about),
+
 ]

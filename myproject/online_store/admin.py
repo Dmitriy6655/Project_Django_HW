77fb708ccd9d96fff2ccc3ba872cee2fdceff701
Timofey_V <tim_vi_ov@mail.ru>
2024-02-15 21:57:26 +0300

    add HW_5 (administrative panel)

--- myproject/online_store/admin.py
+++ myproject/online_store/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Client, Goods, Order
 
-# Register your models here.
+
+# функция для изменения данных
+@admin.action(description="Сбросить количество в ноль")
+def reset_quantity(modeladmin, request, queryset):
+    queryset.update(amount=0)
+
+
+# Колонки отображаемые в админ. панеле Django
+class GoodsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'amount', 'create_at']  # список отображаемых продуктов
+    ordering = ['price', '-amount']  # сортировка
+    list_filter = ['price', 'create_at', 'amount']  # фильтрование
+    search_fields = ['price']
+    search_help_text = "Поиск по цене (price) "
+    actions = [reset_quantity]
+
+    """Отдельный продукт"""
+    fields = ['name', 'price', 'amount', 'create_at']
+    readonly_fields = ['create_at']
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'create_at')
+
+
+
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['client', 'price', 'create_at']
+
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Goods, GoodsAdmin)
+admin.site.register(Order, OrderAdmin)
